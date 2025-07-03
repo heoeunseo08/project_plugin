fae9478764615938db3aa06845a40378c132b70a
heo eunseo <heoeunseong140@gmail.com>
2025-07-03 09:21:00 +0900

    kakao map api connect

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
 
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  bool isloading = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(child: Text('지도')),
+      body: Stack(
+        children: [
+          InAppWebView(
+            initialUrlRequest: URLRequest(url: WebUri("https://map.kakao.com")),
+            onProgressChanged: (controller, progress) {
+              if (mounted) {
+                setState(() {
+                  isloading = progress < 100;
+                });
+              }
+            },
+            onLoadStop: (controller, url) {
+              if (mounted) {
+                setState(() {
+                  isloading = false;
+                });
+              }
+            },
+          ),
+          if (isloading) Center(child: CircularProgressIndicator()),
+        ],
+      ),
     );
   }
 }
